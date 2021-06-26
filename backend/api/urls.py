165796ec52fd80bf6ed15dddd206b70b166d7306
Sigma086 <vineetmahajan2000@gmail.com>
2021-06-26 17:45:17 +0530

    auth system(login only)

---
+++ backend/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    # path('tasks/', views.TaskViewList.as_view(), name='tasks'),
+    # # path('users/', views.TaskViewList.as_view()),
+    # path('tasks/<int:id>/', views.TaskViewDetail.as_view(), name='task'),
+
+    path('auth-token/', obtain_auth_token, name='get_token'),
+    # path('user-register/', views.ReqisterView.as_view(), name='register'),
+]
