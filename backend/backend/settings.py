0390bd611d2ab81cd9a8342507f90ac25e14f659
Sigma086 <vineetmahajan2000@gmail.com>
2021-06-26 20:16:53 +0530

    cors errors resolved

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from . import credentials
+try:
+    from . import credentials
+except:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,6 +56,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
