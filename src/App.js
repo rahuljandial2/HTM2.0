18b3e771f83dd85a96deb01f58a0dd5e2c48923f
rahuljandial2 <61629739+rahuljandial2@users.noreply.github.com>
2021-06-26 21:57:40 +0530

    Created routes for the app
    Now we have two routes
    - /login-page to load the login component
    - /home-page to load the home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Login from './login';
+import Home from './home.js';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login-page" exact component={Login} />
+          <Route path="/home-page" exact component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
