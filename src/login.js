adab99533287ea63b838809c4ccf24cb33984f50
rahuljandial2 <61629739+rahuljandial2@users.noreply.github.com>
2021-06-26 14:31:11 +0530

    Added a basic login page
    All the elements used are material ui and the it doesn't have any handlers at this point of time

---
+++ src/login.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import { Button, TextField } from "@material-ui/core";
+
+class Login extends Component {
+  render() {
+    return (
+      <form className="Login" action="">
+        <h1 className="Login-title">Todo</h1>
+        <TextField className="Login-input" label="Username" variant="outlined" />
+        <TextField className="Login-input" label="Password" variant="outlined" />
+        <div className="Login-btns">
+          <Button className="register" color="primary">Register</Button>
+          <Button className="Login" variant="contained" color="primary">Login</Button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default Login;
