7721eeb7f2934404de0f0a67c21cfccda9f9f601
rahuljandial2 <61629739+rahuljandial2@users.noreply.github.com>
2021-06-27 15:03:33 +0530

    Task check functionality added

--- src/todo.js
+++ src/todo.js
@@ -1,21 +1,38 @@
 import { Component } from "react";
 import { FormControlLabel, Checkbox } from '@material-ui/core';
+import axios from "axios";
 
 class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.handleCheckbox = this.handleCheckbox.bind(this);
+
+    this.state = {
+      isCompleted: this.props.task.is_completed
+    };
+  }
+  handleCheckbox() {
+    const URL = `http://localhost:8000/api/tasks/${this.props.task.id}/`;
+    const TOKEN = localStorage.getItem('token');
+    axios({ method: 'put', url: URL, headers: { "Authorization": `Token ${TOKEN}` }, data: { is_completed: !this.state.isCompleted } })
+      .then(data => {
+        this.setState({ isCompleted: data.data.is_completed })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   render() {
     const task = this.props.task;
     return (
       <div className="Todo">
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={task.is_completed}
-              name="checkedB"
-              color="primary"
-            />
-          }
-          label={task.name}
+        <Checkbox
+          id={task.id}
+          checked={this.state.isCompleted}
+          color="primary"
+          onChange={this.handleCheckbox}
         />
+        <label htmlFor={task.id}>{task.name}</label>
       </div>
     );
   }
