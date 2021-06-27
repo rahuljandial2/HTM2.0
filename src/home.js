449f407152c83629fb75e99b0adb717306e3f02e
rahuljandial2 <61629739+rahuljandial2@users.noreply.github.com>
2021-06-27 11:58:43 +0530

    Rendering of todos
    Now all the todos can be rendered at the home screen but nothing changes yet.

--- src/home.js
+++ src/home.js
@@ -1,11 +1,31 @@
 import { Component } from "react";
 import Todo from './todo.js';
 import { Redirect } from "react-router";
+import axios from "axios";
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.handleLogout = this.handleLogout.bind(this);
+    this.getTasks = this.getTasks.bind(this);
+    this.state = {
+      tasks: []
+    }
+  }
+  getTasks() {
+    const URL = "http://localhost:8000/api/tasks/";
+    const TOKEN = "5cf6991a8f0d44846ee6742e4b2cfab8dda151ba";
+    axios({ method: 'get', url: URL, headers: { "Authorization": `Token ${TOKEN}` } })
+      .then(data => {
+        this.setState({ tasks: data.data });
+        console.log(this.state.tasks);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  componentDidMount() {
+    this.getTasks();
   }
   handleLogout() {
     localStorage.removeItem("token");
@@ -16,8 +36,10 @@ class Home extends Component {
       return (
         <div className="Home">
           <button onClick={this.handleLogout}>Log Out</button>
-          <h1>Todo List</h1>
-          <Todo />
+          <h1 className="Home-title">Todo List</h1>
+          <div className="Home-tasks">
+            {this.state.tasks.map(task => <Todo key={task.id} task={task} />)}
+          </div>
         </div>
       );
     }
